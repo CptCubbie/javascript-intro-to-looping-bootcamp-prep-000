65ea350bf7a8d5ae419decd692e69533ed6ec6ab
Brittany Tinnin <bntinnin7@gmail.com>
2018-03-07 15:44:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,12 +16,3 @@ function whileLoop(n) {
      }
     return 'done';
 }
-
-function doWhileLoop(array) {
-  function maybeTrue() {
-  return Math.random() >= 0.5;
-}
-}
-do { console.log('doo-bee-doo-bee-doo');
-} while (array.length > 0 && maybeTrue());
- return array.pop();
